22d19217204c9640652da8471faf4547706635b8
Ilham Prabaswara <ilham.prabaswara@ai.astra.co.id>
2025-08-05 10:19:24 +0700

    feat: update configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,31 +21,6 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.api.nvim_set_option("clipboard", "unnamedplus")
--- Disable arrow keys in normal mode
-vim.keymap.set('n', '<Up>', '<Nop>')
-vim.keymap.set('n', '<Down>', '<Nop>')
-vim.keymap.set('n', '<Left>', '<Nop>')
-vim.keymap.set('n', '<Right>', '<Nop>')
-
--- Disable arrow keys in insert mode
-vim.keymap.set('i', '<Up>', '<Nop>')
-vim.keymap.set('i', '<Down>', '<Nop>')
-vim.keymap.set('i', '<Left>', '<Nop>')
-vim.keymap.set('i', '<Right>', '<Nop>')
-
--- Disable arrow keys in visual mode
-vim.keymap.set('v', '<Up>', '<Nop>')
-vim.keymap.set('v', '<Down>', '<Nop>')
-vim.keymap.set('v', '<Left>', '<Nop>')
-vim.keymap.set('v', '<Right>', '<Nop>')
-
-local set = vim.opt
-set.relativenumber = true
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
-
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
