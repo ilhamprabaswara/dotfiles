c6d63d4f3b52bc214324de0163756d875b268b2c
Ilham Prabaswara <ilham.prabaswara@ai.astra.co.id>
2025-08-07 14:38:53 +0700

    feat: update configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,24 +1,26 @@
-vim.api.nvim_set_option("clipboard", "unnamedplus")
 -- Disable arrow keys in normal mode
-vim.keymap.set('n', '<Up>', '<Nop>')
-vim.keymap.set('n', '<Down>', '<Nop>')
-vim.keymap.set('n', '<Left>', '<Nop>')
-vim.keymap.set('n', '<Right>', '<Nop>')
+-- vim.keymap.set('n', '<Up>', '<Nop>')
+-- vim.keymap.set('n', '<Down>', '<Nop>')
+-- vim.keymap.set('n', '<Left>', '<Nop>')
+-- vim.keymap.set('n', '<Right>', '<Nop>')
 
 -- Disable arrow keys in insert mode
-vim.keymap.set('i', '<Up>', '<Nop>')
-vim.keymap.set('i', '<Down>', '<Nop>')
-vim.keymap.set('i', '<Left>', '<Nop>')
-vim.keymap.set('i', '<Right>', '<Nop>')
+-- vim.keymap.set('i', '<Up>', '<Nop>')
+-- vim.keymap.set('i', '<Down>', '<Nop>')
+-- vim.keymap.set('i', '<Left>', '<Nop>')
+-- vim.keymap.set('i', '<Right>', '<Nop>')
 
 -- Disable arrow keys in visual mode
-vim.keymap.set('v', '<Up>', '<Nop>')
-vim.keymap.set('v', '<Down>', '<Nop>')
-vim.keymap.set('v', '<Left>', '<Nop>')
-vim.keymap.set('v', '<Right>', '<Nop>')
+-- vim.keymap.set('v', '<Up>', '<Nop>')
+-- vim.keymap.set('v', '<Down>', '<Nop>')
+-- vim.keymap.set('v', '<Left>', '<Nop>')
+-- vim.keymap.set('v', '<Right>', '<Nop>')
+vim.g.have_nerd_font = true
 
-local set = vim.opt
-set.relativenumber = true
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
+vim.opt.termguicolors = true -- True color support
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.tabstop = 2 -- How many spaces are shown per tab
+vim.opt.softtabstop = 2 -- How many spaces are applied  when pressing Tab
+vim.opt.shiftwidth = 2 -- Amount to indent with << and <
